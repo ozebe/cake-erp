99fe15db803dea62d9c3331e3a5686dd7afb7af7
Wesley Ozebe <wesleysantos345@hotmail.com>
2020-02-16 15:13:05 -0300

    alterado para portugues retirado criado/editado

--- templates/GaUsuario/edit.php
+++ templates/GaUsuario/edit.php
@@ -7,20 +7,20 @@
 <div class="row">
     <aside class="column">
         <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
+            <h4 class="heading"><?= __('Ações') ?></h4>
             <?= $this->Form->postLink(
-                __('Delete'),
+                __('Excluir'),
                 ['action' => 'delete', $gaUsuario->id],
                 ['confirm' => __('Are you sure you want to delete # {0}?', $gaUsuario->id), 'class' => 'side-nav-item']
             ) ?>
-            <?= $this->Html->link(__('List Ga Usuario'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('Listar usuários'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
         </div>
     </aside>
     <div class="column-responsive column-80">
         <div class="gaUsuario form content">
             <?= $this->Form->create($gaUsuario) ?>
             <fieldset>
-                <legend><?= __('Edit Ga Usuario') ?></legend>
+                <legend><?= __('Editar usuário') ?></legend>
                 <?php
                     echo $this->Form->control('nome');
                     echo $this->Form->control('usuario');
@@ -28,8 +28,6 @@
                     echo $this->Form->control('bloqueado');
                     echo $this->Form->control('ativo');
                     echo $this->Form->control('tentativas');
-                    echo $this->Form->control('criado');
-                    echo $this->Form->control('editado');
                 ?>
             </fieldset>
             <?= $this->Form->button(__('Submit')) ?>
