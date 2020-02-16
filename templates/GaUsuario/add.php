cb741a6a4e1aecc99165d688db294889bafc49a9
Wesley Ozebe <wesleysantos345@hotmail.com>
2020-02-16 15:09:13 -0300

    idioma da tela add usuario

--- templates/GaUsuario/add.php
+++ templates/GaUsuario/add.php
@@ -7,24 +7,21 @@
 <div class="row">
     <aside class="column">
         <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('List Ga Usuario'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+            <h4 class="heading"><?= __('Ações') ?></h4>
+            <?= $this->Html->link(__('Listar usuários'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
         </div>
     </aside>
     <div class="column-responsive column-80">
         <div class="gaUsuario form content">
             <?= $this->Form->create($gaUsuario) ?>
             <fieldset>
-                <legend><?= __('Add Ga Usuario') ?></legend>
+                <legend><?= __('Cadastrar novo usuário') ?></legend>
                 <?php
                     echo $this->Form->control('nome');
                     echo $this->Form->control('usuario');
                     echo $this->Form->control('senha');
                     echo $this->Form->control('bloqueado');
                     echo $this->Form->control('ativo');
-                    echo $this->Form->control('tentativas');
-                    echo $this->Form->control('criado');
-                    echo $this->Form->control('editado');
                 ?>
             </fieldset>
             <?= $this->Form->button(__('Submit')) ?>
