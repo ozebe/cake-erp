d33bb1d3cb3cc586d60affedb1430e1aaf0eaaa6
Wesley Ozebe <wesleysantos345@hotmail.com>
2020-03-28 08:21:59 -0300

    Adicionado model table de unidade de massa

---
+++ src/Model/Table/GeUnidadeMassaTable.php
@@ -0,0 +1,84 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * GeUnidadeMassa Model
+ *
+ * @method \App\Model\Entity\GeUnidadeMassa get($primaryKey, $options = [])
+ * @method \App\Model\Entity\GeUnidadeMassa newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\GeUnidadeMassa[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\GeUnidadeMassa|false save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\GeUnidadeMassa saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\GeUnidadeMassa patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\GeUnidadeMassa[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\GeUnidadeMassa findOrCreate($search, callable $callback = null, $options = [])
+ */
+class GeUnidadeMassaTable extends Table
+{
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config): void
+    {
+        parent::initialize($config);
+
+        $this->setTable('ge_unidade_massa');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->integer('id')
+            ->allowEmptyString('id', null, 'create');
+
+        $validator
+            ->scalar('descricao')
+            ->maxLength('descricao', 255)
+            ->requirePresence('descricao', 'create')
+            ->notEmptyString('descricao')
+            ->add('descricao', 'unique', ['rule' => 'validateUnique', 'provider' => 'table']);
+
+        $validator
+            ->dateTime('criado')
+            ->requirePresence('criado', 'create')
+            ->notEmptyDateTime('criado');
+
+        $validator
+            ->dateTime('editado')
+            ->allowEmptyDateTime('editado');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules): RulesChecker
+    {
+        $rules->add($rules->isUnique(['descricao']));
+
+        return $rules;
+    }
+}
