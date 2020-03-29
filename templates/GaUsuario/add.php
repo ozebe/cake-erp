861f4e8749aa17f2ad2b4a04167c95d428675522
Wesley Ozebe <wesleysantos345@hotmail.com>
2020-03-29 19:06:46 -0300

    adicionado min length

--- templates/GaUsuario/add.php
+++ templates/GaUsuario/add.php
@@ -14,13 +14,13 @@
 
             echo '<div class="input-field">';
             echo '<i class="material-icons prefix">face</i>';
-            echo $this->Form->text('nome', array("id" =>"nome", "data-length" => "255"));
+            echo $this->Form->text('nome', array("id" =>"nome", "data-length" => "255", "minlength" => "6"));
             echo '<label for="nome">';echo $this->Form->label("nome", "Nome");echo '</label>';
             echo '</div>';
 
             echo '<div class="input-field">';
             echo '<i class="material-icons prefix">account_circle</i>';
-            echo $this->Form->text('usuario', array("id" =>"usuario", "data-length" => "255"));
+            echo $this->Form->text('usuario', array("id" =>"usuario", "data-length" => "255", "minlength" => "3"));
             echo '<label for="usuario">';echo $this->Form->label("usuario", "Usuário");echo '</label>';
             echo '</div>';
 
