f03f4b2d30f2a6799f8646f8e4cd8dbf31064cfc
Wesley Ozebe <wesleysantos345@hotmail.com>
2020-04-12 23:24:26 -0300

    Alterado controlador de geProduto adicionado autorização, adicionado correção para tensão e formas de visualização de estoque, unidade de medida, unidade de massa e outros requiridos.

--- src/Controller/GeProdutoController.php
+++ src/Controller/GeProdutoController.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Cake\Http\Exception\ForbiddenException;
+
 /**
  * GeProduto Controller
  *
@@ -12,6 +14,16 @@ namespace App\Controller;
  */
 class GeProdutoController extends AppController
 {
+    public function autorizado(){
+        $aut = false;
+        foreach($this->nivelUsuario as $nivel){
+            if(($nivel->ga_nivel_acesso->sigla == "ADM") OR ($nivel->ga_nivel_acesso->sigla == "GE")){
+                $aut = true;
+                break;
+            }
+        }
+        return $aut;
+    }
     /**
      * Index method
      *
@@ -19,9 +31,12 @@ class GeProdutoController extends AppController
      */
     public function index()
     {
-        $geProduto = $this->paginate($this->GeProduto);
-
+        if($this->autorizado()){
+        $geProduto = $this->paginate($this->GeProduto->find('all')->contain('GeSubGrupoProd'));
         $this->set(compact('geProduto'));
+        }else {
+            throw new ForbiddenException(__('Você não possui acesso a este módulo'));
+        }
     }
 
     /**
@@ -33,11 +48,15 @@ class GeProdutoController extends AppController
      */
     public function view($id = null)
     {
-        $geProduto = $this->GeProduto->get($id, [
-            'contain' => [],
-        ]);
+        if($this->autorizado()){
+            $geProduto = $this->GeProduto->get($id, [
+                'contain' => ['GeSubGrupoProd', 'GeEstoque', 'GeLote', 'GeUnidadeMassa', 'GeUnidadeMedida'],
+            ]);
 
-        $this->set('geProduto', $geProduto);
+            $this->set('geProduto', $geProduto);
+        }else {
+            throw new ForbiddenException(__('Você não possui acesso a este módulo'));
+        }
     }
 
     /**
@@ -47,17 +66,66 @@ class GeProdutoController extends AppController
      */
     public function add()
     {
+        if($this->autorizado()){
         $geProduto = $this->GeProduto->newEmptyEntity();
         if ($this->request->is('post')) {
             $geProduto = $this->GeProduto->patchEntity($geProduto, $this->request->getData());
+            //se a tensão não tiver nada ele coloca como null
+            if($geProduto->tensao == ''){
+                $geProduto->tensao = null;
+            }elseif ($geProduto->tensao == 0){
+                $geProduto->tensao = '110V';
+            }elseif ($geProduto->tensao == 1){
+                $geProduto->tensao = '220V';
+            }elseif ($geProduto->tensao == 2){
+                $geProduto->tensao = 'Outros';
+            }
             if ($this->GeProduto->save($geProduto)) {
-                $this->Flash->success(__('The ge produto has been saved.'));
+                $this->Flash->success(__('Produto salvo com sucesso'));
 
                 return $this->redirect(['action' => 'index']);
             }
-            $this->Flash->error(__('The ge produto could not be saved. Please, try again.'));
+            $this->Flash->error(__('O Produto não pode ser salvo. Por favor tente novamente!'));
         }
         $this->set(compact('geProduto'));
+
+        $unidadeMedida = $this->GeProduto->GeUnidadeMedida->find('list',[
+            'keyField' => 'id',
+            'valueField' => 'descricao',
+            'order' => 'descricao'
+        ]);
+        $this->set(compact('unidadeMedida'));
+
+        $unidadeMassa = $this->GeProduto->GeUnidadeMassa->find('list',[
+            'keyField' => 'id',
+            'valueField' => 'descricao',
+            'order' => 'descricao'
+        ]);
+        $this->set(compact('unidadeMassa'));
+
+        $subGrupo = $this->GeProduto->GeSubGrupoProd->find('list',[
+            'keyField' => 'id',
+            'valueField' => 'descricao',
+            'order' => 'id_ge_grupo_prod'
+        ]);
+        $this->set(compact('subGrupo'));
+
+        $estoque = $this->GeProduto->GeEstoque->find('list',[
+            'keyField' => 'id',
+            'valueField' => 'descricao',
+            'order' => 'descricao'
+        ]);
+        $this->set(compact('estoque'));
+
+        $lote = $this->GeProduto->GeLote->find('list',[
+            'keyField' => 'id',
+            'valueField' => 'num_lote',
+            'order' => ['criado' => 'DESC']
+        ]);
+        $this->set(compact('lote'));
+        }else {
+            throw new ForbiddenException(__('Você não possui acesso a este módulo'));
+        }
     }
 
     /**
@@ -69,19 +137,69 @@ class GeProdutoController extends AppController
      */
     public function edit($id = null)
     {
+        if($this->autorizado()){
         $geProduto = $this->GeProduto->get($id, [
             'contain' => [],
         ]);
         if ($this->request->is(['patch', 'post', 'put'])) {
             $geProduto = $this->GeProduto->patchEntity($geProduto, $this->request->getData());
+            //se a tensão não tiver nada ele coloca como null
+            if($geProduto->tensao == ''){
+                $geProduto->tensao = null;
+            }elseif ($geProduto->tensao == 0){
+                $geProduto->tensao = '110V';
+            }elseif ($geProduto->tensao == 1){
+                $geProduto->tensao = '220V';
+            }elseif ($geProduto->tensao == 2){
+                $geProduto->tensao = 'Outros';
+            }
             if ($this->GeProduto->save($geProduto)) {
-                $this->Flash->success(__('The ge produto has been saved.'));
+                $this->Flash->success(__('Produto editado com sucesso'));
 
                 return $this->redirect(['action' => 'index']);
             }
-            $this->Flash->error(__('The ge produto could not be saved. Please, try again.'));
+            $this->Flash->error(__('O Produto não pode se editado. Por favor tente novamente!'));
         }
         $this->set(compact('geProduto'));
+
+        $unidadeMedida = $this->GeProduto->GeUnidadeMedida->find('list',[
+            'keyField' => 'id',
+            'valueField' => 'descricao',
+            'order' => 'descricao'
+        ]);
+        $this->set(compact('unidadeMedida'));
+
+        $unidadeMassa = $this->GeProduto->GeUnidadeMassa->find('list',[
+            'keyField' => 'id',
+            'valueField' => 'descricao',
+            'order' => 'descricao'
+        ]);
+        $this->set(compact('unidadeMassa'));
+
+        $subGrupo = $this->GeProduto->GeSubGrupoProd->find('list',[
+            'keyField' => 'id',
+            'valueField' => 'descricao',
+            'order' => 'id_ge_grupo_prod'
+        ]);
+        $this->set(compact('subGrupo'));
+
+        $estoque = $this->GeProduto->GeEstoque->find('list',[
+            'keyField' => 'id',
+            'valueField' => 'descricao',
+            'order' => 'descricao'
+        ]);
+        $this->set(compact('estoque'));
+
+        $lote = $this->GeProduto->GeLote->find('list',[
+            'keyField' => 'id',
+            'valueField' => 'num_lote',
+            'order' => ['criado' => 'DESC']
+        ]);
+        $this->set(compact('lote'));
+        }else {
+            throw new ForbiddenException(__('Você não possui acesso a este módulo'));
+        }
+
     }
 
     /**
@@ -93,14 +211,18 @@ class GeProdutoController extends AppController
      */
     public function delete($id = null)
     {
+        if($this->autorizado()){
         $this->request->allowMethod(['post', 'delete']);
         $geProduto = $this->GeProduto->get($id);
         if ($this->GeProduto->delete($geProduto)) {
-            $this->Flash->success(__('The ge produto has been deleted.'));
+            $this->Flash->success(__('O Produto foi excluído'));
         } else {
-            $this->Flash->error(__('The ge produto could not be deleted. Please, try again.'));
+            $this->Flash->error(__('O produto não pode ser excluído. Por favor tente novamente'));
         }
 
         return $this->redirect(['action' => 'index']);
+        }else {
+            throw new ForbiddenException(__('Você não possui acesso a este módulo'));
+        }
     }
 }
