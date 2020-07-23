998f1577a7714715e03c9dc8cf611b78ae1a9b64
Wesley Ozebe <wesleysantos345@hotmail.com>
2020-07-22 21:20:29 -0300

    Adicionado Table model de RhTipoProfissao

---
+++ src/Model/Table/RhTipoProfissaoTable.php
@@ -0,0 +1,90 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * RhTipoProfissao Model
+ *
+ * @property \App\Model\Table\RhAreaProfissaoTable&\Cake\ORM\Association\BelongsTo $RhAreaProfissao
+ *
+ * @method \App\Model\Entity\RhTipoProfissao get($primaryKey, $options = [])
+ * @method \App\Model\Entity\RhTipoProfissao newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\RhTipoProfissao[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\RhTipoProfissao|false save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\RhTipoProfissao saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\RhTipoProfissao patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\RhTipoProfissao[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\RhTipoProfissao findOrCreate($search, callable $callback = null, $options = [])
+ */
+class RhTipoProfissaoTable extends Table
+{
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config): void
+    {
+        parent::initialize($config);
+
+        $this->setTable('rh_tipo_profissao');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+
+        $this->belongsTo('RhAreaProfissao', [
+            'foreignKey' => 'rh_area_prof_id',
+            'joinType' => 'INNER',
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->integer('id')
+            ->allowEmptyString('id', null, 'create');
+
+        $validator
+            ->scalar('descricao')
+            ->maxLength('descricao', 255)
+            ->requirePresence('descricao', 'create')
+            ->notEmptyString('descricao');
+
+        $validator
+            ->dateTime('criado')
+            ->requirePresence('criado', 'create')
+            ->notEmptyDateTime('criado');
+
+        $validator
+            ->dateTime('editado')
+            ->allowEmptyDateTime('editado');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules): RulesChecker
+    {
+        $rules->add($rules->existsIn(['rh_area_prof_id'], 'RhAreaProfissao'));
+
+        return $rules;
+    }
+}
